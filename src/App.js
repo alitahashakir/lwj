4cb0b5749ce0a1020b876165143e79dae6b3c9e1
unknown <alitahashakir@gmail.com>
2020-09-23 23:11:57 +0500

    made the box spin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React, { useState } from "react";
-import { Canvas } from "react-three-fiber";
+import React, { useState, useRef } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import { useSpring, a } from "react-spring/three";
 import "./App.css";
 
 const Box = () => {
+  const meshRef = useRef();
   const [hovered, setHovered] = useState(false);
   const [active, setActive] = useState(false);
+
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    color: hovered ? "hotpink" : "gray",
+  });
+
+  useFrame(() => {
+    meshRef.current.rotation.y += 0.01;
+  });
+
+  console.log("hello world");
+
   return (
-    <mesh
+    <a.mesh
+      ref={meshRef}
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
       onClick={() => setActive(!active)}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      scale={props.scale}
     >
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshBasicMaterial
-        attach="material"
-        color={hovered ? "hotpink" : "gray"}
-      />
-    </mesh>
+      <a.meshBasicMaterial attach="material" color={props.color} />
+    </a.mesh>
   );
 };
 
